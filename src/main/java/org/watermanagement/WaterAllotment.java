f430af9e464b486529d1ad2fba05ddb64d378140
Emily Namugaanyi <enamugaanyi@squareup.com>
2024-02-15 10:26:06 +1100

    Calculate standard cost of water for 3 bedroom

--- src/main/java/org/watermanagement/WaterAllotment.java
+++ src/main/java/org/watermanagement/WaterAllotment.java
@@ -1,13 +1,23 @@
 package org.watermanagement;
 
 public class WaterAllotment {
-  public double calculateStandardCost(int corporationRatio, int borewellRatio) {
+  public double calculateStandardCost(int apartmentType,int corporationRatio, int borewellRatio) {
+    if(apartmentType == 2){
+      return getStandardAmount(corporationRatio, borewellRatio, 900);
+    }else if (apartmentType == 3) {
+      return getStandardAmount(corporationRatio, borewellRatio, 1500);
+    }else{
+      throw new IllegalArgumentException("There is something strange in the input");
+    }
+  }
+
+  private static double getStandardAmount(int corporationRatio, int borewellRatio, int waterAlloted) {
     int denominator = corporationRatio + borewellRatio;
     double corpFraction = (double) corporationRatio / denominator;
     double borewellFraction = (double) borewellRatio /denominator;
 
-    double amountForCorp = corpFraction * 900 * 1.0;
-    double amountForBore = borewellFraction * 900 * 1.5;
+    double amountForCorp = corpFraction * waterAlloted * 1.0;
+    double amountForBore = borewellFraction * waterAlloted * 1.5;
 
     return amountForCorp + amountForBore;
   }
--- src/test/java/WaterAllotmentTest.java
+++ src/test/java/WaterAllotmentTest.java
@@ -6,7 +6,21 @@ public class WaterAllotmentTest {
   @Test
   public void testGetAmountForTwoBedRoom(){
     WaterAllotment testInstance = new WaterAllotment();
-    double actual = testInstance.calculateStandardCost(1, 2);
+    double actual = testInstance.calculateStandardCost(2,1, 2);
     Assertions.assertEquals(1200, actual, 0.001);
   }
+
+  @Test
+  public void testGetAmountForThreeBedRoom(){
+    WaterAllotment testInstance = new WaterAllotment();
+    double actual = testInstance.calculateStandardCost(3, 2, 3);
+    Assertions.assertEquals(1950, actual, 0.001);
+  }
+
+  //@Test
+  //public void testThrowExceptionWhenApartmentTypeIsInvalid(){
+  //  WaterAllotment testInstance = new WaterAllotment();
+  //  double actual = testInstance.calculateStandardCost(4, 2, 3);
+  //  Assertions.assertEquals(1950, actual, 0.001);
+  //}
 }
