3a53ac698b97e46d865935a031427098dc0e666c
Emily Namugaanyi <enamugaanyi@squareup.com>
2024-02-15 10:53:09 +1100

    Calculate water consumed in 3 bed room apartment

--- src/main/java/org/watermanagement/ThreeBedRoom.java
+++ src/main/java/org/watermanagement/ThreeBedRoom.java
@@ -1,2 +1,12 @@
-package org.watermanagement;public class ThreeBedRoom {
+package org.watermanagement;
+
+
+public class ThreeBedRoom {
+  private static final int WATER_ALLOTTED = 1500;
+  private static final int LITRES_ALLOTED_PER_GUEST = 10;
+  private static final int NUMBER_OF_DAYS_IN_MONTH = 30;
+
+  public int getWaterConsumed(int numberOfGuests) {
+    return WATER_ALLOTTED + (numberOfGuests * LITRES_ALLOTED_PER_GUEST * NUMBER_OF_DAYS_IN_MONTH);
+  }
 }
--- src/test/java/ThreeBedroomTest.java
+++ src/test/java/ThreeBedroomTest.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ThreeBedroomTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.watermanagement.ThreeBedRoom;
+
+public class ThreeBedroomTest {
+  @Test
+  public void testGetWaterConsumedForThreeBedRoom(){
+    int actual = new ThreeBedRoom().getWaterConsumed(2);
+    Assertions.assertEquals(2100, actual );
+  }
 }
